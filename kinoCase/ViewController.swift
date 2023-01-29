fd124e72b78090cefdff2e550ade30d50544ba50
Kamskiy <123025576+Kamskiy@users.noreply.github.com>
2023-01-29 17:46:01 +0300

    Layout
    исправлена верстка

--- kinoCase/ViewController.swift
+++ kinoCase/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var disAgreeButton: UIButton!
+    @IBOutlet weak var agreeButton: UIButton!
+    @IBOutlet weak var qustionLable: UILabel!
+    @IBOutlet weak var numberLable: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.layer.cornerRadius = 15
+        disAgreeButton.layer.cornerRadius = 15
+        agreeButton.layer.cornerRadius = 15
+
+        
     }
 
 
